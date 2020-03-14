fa962f0f94dabb9f1c4b79a555b6c377773477ce
Anthony Kawa <anthonyk2020@gmail.com>
2020-03-14 16:41:47 -0700

    MVP goals: completed MVP

--- src/App.js
+++ src/App.js
@@ -1,48 +1,83 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { 
+  addFeature, 
+  updateTotal, 
+  removeFeature, 
+  removeAdditonalFeature, 
+  addAdditionalFeature
+} from './actions/carSalesActions';
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
-const App = () => {
+const App = (props) => {
+  console.log(props);
+  const car = props.car;
+  const additionalPrice = props.additionalPrice;
+  const additionalFeatures = props.additionalFeatures;
+  
   const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
+    
   };
 
-  const removeFeature = item => {
+  const unBuyItem = item => {
     // dispatch an action here to remove an item
+    props.removeFeature(item.id);
+    const newItem = {
+      ...item,
+      price: -item.price
+    }
+    props.addAdditionalFeature(item);
+    props.updateTotal(newItem);
   };
 
   const buyItem = item => {
     // dipsatch an action here to add an item
+    props.addFeature(item);
+    props.removeAdditonalFeature(item.id);
+    props.updateTotal(item);
   };
 
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={car} />
+        <AddedFeatures car={car} unBuyItem={unBuyItem} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures 
+          additionalFeatures={additionalFeatures}
+          addFeature={buyItem} />
+        <Total car={car} additionalPrice={additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = (state) => {
+  const {price, name, image, features} = state.carSalesReducer;
+  const {additionalPrice} = state.additionalPriceReducer;
+  const additionalFeatures = state.additionalFeatureReducer;
+  console.log(state)
+  return {
+    car:{
+      price,
+      name,
+      image,
+      features
+    },
+    additionalPrice,
+    additionalFeatures
+  }
+}
+
+export default connect(mapStateToProps, {
+  addFeature,
+  updateTotal,
+  removeFeature,
+  removeAdditonalFeature,
+  addAdditionalFeature
+})(App);
---
+++ src/actions/carSalesActions.js
@@ -0,0 +1,34 @@
+export function addFeature(feature){
+    return {
+        type: 'ADD_FEATURE',
+        payload: feature
+    }
+}
+
+export function updateTotal(item){
+    return {
+        type: 'UPDATE_TOTAL',
+        payload: item.price
+    }
+}
+
+export function removeFeature(id){
+    return {
+        type: 'REMOVE_FEATURE',
+        payload: id
+    }
+}
+
+export function removeAdditonalFeature(id){
+    return {
+        type: 'REMOVE_ADDITIONAL_FEATURE',
+        payload: id
+    }
+}
+
+export function addAdditionalFeature(item){
+    return {
+        type: 'ADD_ADDITIONAL_FEATURE',
+        payload: item
+    }
+}
